d0e3e25255795bef405af95a453a64465c3d7950
Gonuguntla Udaya Kiran <udaya.kirangonuguntla.20cse@bmu.edu.in>
2022-05-15 10:12:48 +0530

    Minor changes

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
-router.post('/',async (req,res)=>{
+router.post('/add-user',async (req,res)=>{
     let user = new User({
         name:req.body.name,
         email:req.body.email
